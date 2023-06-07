e681b607284ae02bc093bb502708699b2f409a86
ilyaputrich <ilya.putrich@mail.ru>
2023-06-07 20:20:04 +0300

    1

---
+++ README.md
@@ -0,0 +1,3 @@
+"# blog-django"  
+"# blog-django"  
+"# blog" 
---
+++ myblog/basic/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+from django.views.generic import ListView
+from .models import Post
+from .form import CommentsForm
+# from django.contrib.postgres.search import SearchVector
+
+class PostView(View):
+    "Вывод записей"
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, "basic/basic.html", {"post_list": posts})
+
+class PostDetail(View):
+    "отдельная страница записи"
+    def get(self, request, pk):
+        post = Post.objects.get(id=pk)
+        return render(request, "basic/blog_detail.html", {"post": post})
+class AddComments(View):
+    "добавление коментариев"
+    def post(self, request, pk):
+        form = CommentsForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.post_id = pk
+            form.save()
+        return redirect(f"/{pk}")
+
+class Search(View):
+    "Вывод записей"
+    def get(self, request):
+
+        posts = Post.objects.filter(author__contains=self.request.GET.get("search"))
+        return render(request, "basic/basic.html", {"post_list": posts})
+
+# class Search(View):
+#     "Вывод записей"
+#     def get(self, request):
+#
+#         posts = Post.objects.annotate(search=SearchVector("title","descriptions","author"),
+#                                       ).filter(search=self.request.GET.get("search"))
+#         print(posts)
+#         return render(request, "basic/basic.html", {"post_list": posts})
+
+
